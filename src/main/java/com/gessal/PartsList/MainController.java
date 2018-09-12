fd43154909d9614749a4701d460942d6a3ed5983
gessal <gessalmaxim@gmail.com>
2018-09-12 12:58:21 +0300

    Прикрутил поиск. Пытаюсь прикрутить форму редактирования.

--- src/main/java/com/gessal/PartsList/MainController.java
+++ src/main/java/com/gessal/PartsList/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
 
 @Controller
@@ -41,34 +40,30 @@ public class MainController {
 
     /******************Редактирование записи********************/
     @GetMapping(path="/edit")
-    public @ResponseBody String editPart (@RequestParam String id, @RequestParam String name, @RequestParam String is_need, @RequestParam String count) {
+    public @ResponseBody String editPart (@RequestParam String id, Model model) {
         Part part = partRepository.findById(Integer.parseInt(id)).get();
-        part.setName(name);
-        if (is_need.equals("true")) {
-            part.setIs_need(true);
-        } else {
-            part.setIs_need(false);
-        }
-        part.setCount(Integer.parseInt(count));
-        partRepository.save(part);
-        return "Changed";
-    }
-
-    /******************Поиск записи********************/
-    @GetMapping(path="/find")
-    public @ResponseBody Iterable<Part> findPart (@RequestParam String name) {
-        return partRepository.findByName(name);
+        model.addAttribute("name", part.getName());
+        model.addAttribute("is_need", part.getIs_need());
+        model.addAttribute("count", part.getCount());
+        return "editPage";
     }
 
     /******************Вывод всех записей********************/
     @GetMapping(path="/all")
-    public String getAllParts(@RequestParam(name="page", required=false, defaultValue="1") String pageN, Model model) {
+    public String getAllParts(@RequestParam(name="page", required=false, defaultValue="1") String pageN, @RequestParam(name="find", required=false, defaultValue="") String findName, Model model) {
         Pageable page = new PageRequest(Integer.parseInt(pageN)-1, 10);
-        Page<Part> partsPage = partRepository.findAll(page);
+        Page<Part> partsPage;
+        if (findName.equals("")) {
+            partsPage = partRepository.findAll(page);
+        } else {
+            partsPage = partRepository.findByName(findName, page);
+        }
         int[] pages = new int[partsPage.getTotalPages()];
         for (int i = 0; i < pages.length; i++) {
             pages[i] = i+1;
         }
+        //--------------------
+        model.addAttribute("find", findName);
         model.addAttribute("pages", pages);
         model.addAttribute("parts", partsPage);
         return "mainPage";
--- src/main/java/com/gessal/PartsList/PartRepository.java
+++ src/main/java/com/gessal/PartsList/PartRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 
@@ -13,6 +12,6 @@ import java.util.Optional;
 
 public interface PartRepository extends CrudRepository<Part, Integer> {
     Optional<Part> findById(Integer id);
-    List<Part> findByName(String name);
+    Page<Part> findByName(String name, Pageable page);
     Page<Part> findAll(Pageable pageable);
 }
